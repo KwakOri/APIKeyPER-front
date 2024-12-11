80cae9649bb569d3cfd9d38a500cee4eaa68f0bb
kwakjaehun <jh5414092@icloud.com>
2024-12-12 00:39:31 +0900

    Feat: components/Button 작성

--- src/app/(root)/test/page.tsx
+++ src/app/(root)/test/page.tsx
@@ -1,11 +1,17 @@
+import Button from "@/components/Button";
 import Input from "@/components/Input";
 
 const page = () => {
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="w-[375px] h-[660px] bg-white">
-        <div className={"px-4"}>
-          <Input label="email" placeholder={"apikeyper@keeper.com"} />
+        <div className={"px-4 flex flex-col gap-4"}>
+          <Input
+            label="email"
+            id={"email"}
+            placeholder={"apikeyper@keeper.com"}
+          />
+          <Button>LOGIN</Button>
         </div>
       </div>
     </div>
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,19 +2,22 @@ import { InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  id: string;
 }
 
-const Input = ({ label, ...props }: InputProps) => {
+const Input = ({ id, label, ...props }: InputProps) => {
   return (
-    <div>
-      <label className={"text-sm text-primary-55"} htmlFor={label}>
-        {label}
-      </label>
+    <div className="flex flex-col gap-1 w-full h-full">
+      {label && (
+        <label className={"text-sm text-primary-55"} htmlFor={id}>
+          {label}
+        </label>
+      )}
       <input
         className={
           "text-sm h-11 w-full px-4 placeholder-primary-55 text-primary-75 rounded-lg border border-primary-55"
         }
-        id={label}
+        id={id}
         {...props}
       />
     </div>
