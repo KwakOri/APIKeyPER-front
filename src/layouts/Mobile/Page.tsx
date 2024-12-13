ae52a4157b371f6196bb4444a352b79230b69be2
kwakjaehun <jh5414092@icloud.com>
2024-12-14 01:57:09 +0900

    Style: layout 수정

--- src/layouts/Mobile/Page.tsx
+++ src/layouts/Mobile/Page.tsx
@@ -2,12 +2,10 @@ import { PropsWithChildren } from "react";
 
 const Mobile = ({ children }: PropsWithChildren) => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div
-        className={"w-[375px] h-[660px] shrink-0 bg-white rounded-lg  relative"}
-      >
-        <div className="w-full h-full overflow-y-scroll ">{children}</div>
-      </div>
+    <div className="w-full h-full flex justify-center fixed overflow-hidden ">
+      <section className="h-full w-full flex flex-col max-w-[600px] relative bg-white">
+        <div className="w-full h-full overflow-y-scroll">{children}</div>
+      </section>
     </div>
   );
 };
